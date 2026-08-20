b7aa0ae3db178722ab1abeea0577462f9d71c487
Srilakshmi Sripathi <43179518+SrilakshmiSripathi@users.noreply.github.com>
2026-08-20 17:40:51 -0400

    vite preview updated

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  preview: {
+    allowedHosts: ['planify-academic-resource-career.onrender.com']
+  }
 })
